9ba9aa2c2e6c93712d37daa8ead632bf9e40125c
André Sousa <afssa11111@iscte-iul.pt>
2018-03-17 23:51:34 +0000

    Method to instantiate problem from existing XML problem.

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,11 +13,15 @@ import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathFactory;
 import java.io.File;
+import java.io.FileNotFoundException;
 
 
-
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
         //SolverandOptimizer.getInstance();
@@ -26,11 +30,12 @@ public class Main{
         Variable[] vars = {new Variable("a", 4), new Variable("p", 0.0), new Variable("v", false), new Variable("c", false)};
         Problem p = new Problem("Test Problem", "Test problem relativo a criacao de xml", vars);
 
-
+        saveProblemXML("test", p);
+        Problem p_r = readXMLProblem("test");
 
     }
 
-    public void saveProblemXML(String filename, Problem p) {
+    public static void saveProblemXML(String filename, Problem p) {
         try {
 
             DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
@@ -46,28 +51,24 @@ public class Main{
             rootElement.appendChild(staff);
 
             // set attribute to problem element
-            Attr attr = doc.createAttribute("name");
-            attr.setValue(p.getProblemName());
-            staff.setAttributeNode(attr);
+            staff.setAttribute("description", p.getProblemDescription());
+            staff.setAttribute("name", p.getProblemName());
 
-
-            // description elements
-            Element description = doc.createElement("description");
-            description.appendChild(doc.createTextNode(p.getProblemDescription()));
-            staff.appendChild(description);
+            Element variables = doc.createElement("Variables");
+            rootElement.appendChild(variables);
 
             // variable elements
-            for(int i = 0; i < p.getVariables().length; i++) {
+            for (int i = 0; i < p.getVariables().length; i++) {
                 Element variable = doc.createElement(p.getVariables()[i].getVariableName().replace(" ", "_"));
-                variable.appendChild(doc.createTextNode(p.getVariables()[i].getVariable().toString()));
-                staff.appendChild(variable);
+                variable.appendChild(doc.createTextNode(p.getVariables()[i].getVariable().toString().replace(" ", "_")));
+                variables.appendChild(variable);
             }
 
             // write the content into xml file
             TransformerFactory transformerFactory = TransformerFactory.newInstance();
             Transformer transformer = transformerFactory.newTransformer();
             DOMSource source = new DOMSource(doc);
-            StreamResult result = new StreamResult(new File(System.getProperty("user.dir") + filename));
+            StreamResult result = new StreamResult(new File(System.getProperty("user.dir") + "\\" + filename + ".xml"));
 
             // Output to console for testing
             // StreamResult result = new StreamResult(System.out);
@@ -82,4 +83,47 @@ public class Main{
             tfe.printStackTrace();
         }
     }
+
+    public static Problem readXMLProblem(String filename) {
+        try {
+            String p_description = "";
+            String p_name = "";
+
+
+            File config = new File(System.getProperty("user.dir") + "\\" + filename + ".xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(config);
+            doc.getDocumentElement().normalize();
+
+            XPathFactory xpathFactory = XPathFactory.newInstance();
+            XPath xpath = xpathFactory.newXPath();
+            XPathExpression expr = xpath.compile("/root/Problem/@*");
+
+            NodeList nl = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+
+            for (int i = 0; i < nl.getLength(); i++) {
+                if (nl.item(i).getNodeName().equals("description"))
+                    p_description = nl.item(i).getNodeValue();
+                if (nl.item(i).getNodeName().equals("name"))
+                    p_name = nl.item(i).getNodeValue();
+            }
+
+            expr = xpath.compile("/root/Variables/*");
+            nl = (NodeList) expr.evaluate(doc, XPathConstants.NODESET);
+            Variable[] vars_l = new Variable[nl.getLength()];
+            for (int i = 0; i < nl.getLength(); i++) {
+                Variable v = new Variable(nl.item(i).getNodeName(), nl.item(i).getFirstChild().getNodeValue().toString());
+                vars_l[i] = v;
+            }
+
+            return new Problem(p_name, p_description, vars_l);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Make sure configuration file is available.");
+        } catch (Exception e) {
+        }
+        return null;
+    }
+
 }
