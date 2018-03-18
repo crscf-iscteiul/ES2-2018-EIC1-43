17e6c9e33a2c244a5c84cefba12b50992891296d
rafaelscfernandes <rafaelscfernandes@hotmail.com>
2018-03-18 02:03:19 +0000

    Mail sender created

---
+++ src/utils/Mail.java
@@ -0,0 +1,41 @@
+package utils;
+
+import javax.mail.*;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Mail {
+
+    private static final String username = "problemsolverandoptimizer@gmail.com";
+    private static final String password = "jukojuko69";
+
+    public static void sendMail(String to, String subject, String text){
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+        });
+
+        try{
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(subject);
+            message.setText(text);
+
+            Transport.send(message);
+        } catch (AddressException e) {
+
+        } catch (MessagingException e) {
+
+        }
+    }
+}
