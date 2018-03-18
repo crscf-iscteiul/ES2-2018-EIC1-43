d0da08a583d1ae480ea86560d3f63edf86c85cac
rafaelscfernandes <rafaelscfernandes@hotmail.com>
2018-03-18 02:24:16 +0000

    Changed the way sending and email works

--- src/utils/Mail.java
+++ src/utils/Mail.java
@@ -1,5 +1,8 @@
 package utils;
 
+import main.Main;
+import solver.SolverandOptimizer;
+
 import javax.mail.*;
 import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
@@ -8,22 +11,32 @@ import java.util.Properties;
 
 public class Mail {
 
-    private static final String username = "problemsolverandoptimizer@gmail.com";
-    private static final String password = "jukojuko69";
+    private static Mail instance;
+    public static Mail getInstance() {
+        if (instance == null)
+            instance = new Mail();
+        return instance;
+    }
 
-    public static void sendMail(String to, String subject, String text){
+    private final String username = "problemsolverandoptimizer@gmail.com";
+    private final String password = "jukojuko69";
+    private Session session;
+
+    private Mail(){
         Properties props = new Properties();
         props.put("mail.smtp.starttls.enable", "true");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
 
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
+        session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
         });
+    }
 
+    public void sendMail(String to, String subject, String text){
         try{
             Message message = new MimeMessage(session);
             message.setFrom(new InternetAddress(username));
